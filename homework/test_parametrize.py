6795855d7afc8800322091ee782f0e05dac64cb2
Sergey Ikonnikov <s.ikonnikov@profeelab.net>
2025-06-25 23:23:10 +0800

    test_parametrize.py done

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -1,17 +1,22 @@
-"""
-Переопределите параметр с помощью indirect параметризации на уровне теста
-"""
 import pytest
+from selene import browser, have
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[(1920, 1080), (1280, 1024), (375, 665), (430, 932)], ids=["1920x180", "1280x1024","375x665","430x932"])
+def browser_config(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    yield
+    browser.quit()
 
+@pytest.mark.parametrize("browser_config",[(1920, 1080), (1280, 1024)],ids=["1920x1080", "1280x1024"],indirect=True)
+def test_github_desktop(browser_config):
+    browser.open("https://github.com/")
+    browser.all('button').element_by(have.text('Sign up')).click()
 
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize("browser_config",[(375, 665), (430, 932)],ids=["375x665", "430x932"],indirect=True)
+def test_github_mobile(browser_config):
+    browser.open("https://github.com/")
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-up').click()
