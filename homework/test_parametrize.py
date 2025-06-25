0a009ce459d11c853e99c5dc3b1a497f763a0d6c
Sergey Ikonnikov <s.ikonnikov@profeelab.net>
2025-06-25 23:30:24 +0800

    homework done

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -2,7 +2,8 @@ import pytest
 from selene import browser, have
 
 
-@pytest.fixture(params=[(1920, 1080), (1280, 1024), (375, 665), (430, 932)], ids=["1920x180", "1280x1024","375x665","430x932"])
+@pytest.fixture(params=[(1920, 1080), (1280, 1024), (375, 665), (430, 932)],
+                ids=["1920x180", "1280x1024", "375x665", "430x932"])
 def browser_config(request):
     width, height = request.param
     browser.config.window_width = width
@@ -10,12 +11,14 @@ def browser_config(request):
     yield
     browser.quit()
 
-@pytest.mark.parametrize("browser_config",[(1920, 1080), (1280, 1024)],ids=["1920x1080", "1280x1024"],indirect=True)
+
+@pytest.mark.parametrize("browser_config", [(1920, 1080), (1280, 1024)], ids=["1920x1080", "1280x1024"], indirect=True)
 def test_github_desktop(browser_config):
     browser.open("https://github.com/")
     browser.all('button').element_by(have.text('Sign up')).click()
 
-@pytest.mark.parametrize("browser_config",[(375, 665), (430, 932)],ids=["375x665", "430x932"],indirect=True)
+
+@pytest.mark.parametrize("browser_config", [(375, 665), (430, 932)], ids=["375x665", "430x932"], indirect=True)
 def test_github_mobile(browser_config):
     browser.open("https://github.com/")
     browser.element('.Button-content').click()
--- homework/test_skip.py
+++ homework/test_skip.py
@@ -1,18 +1,31 @@
-"""
-Параметризуйте фикстуру несколькими вариантами размеров окна
-Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
-"""
 import pytest
+from selene import browser, have
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[(1920, 1080), (1280, 1024), (375, 665), (430, 932)],
+                ids=["1920x180", "1280x1024", "375x665", "430x932"])
+def browser_config(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    if width <= 430:
+        yield "mobile"
+        browser.quit()
+    else:
+        yield "desktop"
+        browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+def test_github_desktop(browser_config):
+    if browser_config == "mobile":
+        pytest.skip(reason="screen resolution for mobile devices")
+    browser.open("https://github.com/")
+    browser.all('button').element_by(have.text('Sign up')).click()
 
 
-def test_github_mobile(browser):
-    pass
+def test_github_mobile(browser_config):
+    if browser_config == "desktop":
+        pytest.skip(reason="Desktop screen resolution")
+    browser.open("https://github.com/")
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-up').click()
