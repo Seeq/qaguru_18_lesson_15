6707040d0ee06eaebefa8071c06813167c36273a
Sergey Ikonnikov <s.ikonnikov@profeelab.net>
2025-06-25 23:11:08 +0800

    test_fixtures.py done

--- homework/test_fixtures.py
+++ homework/test_fixtures.py
@@ -1,44 +1,31 @@
-"""
-Сделайте разные фикстуры для каждого теста, которые выставят размеры окна браузера
-"""
-import os
 import pytest
-from selene import browser
-from selenium import webdriver
+from selene import browser, have
 
-@pytest.fixture(scope='function')
-def browser_desktop():
-    browser.config.window_height = 800
-    browser.config.window_width = 1280
-    browser.config.base_url = 'https://github.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
 
+@pytest.fixture(params=[(1920, 1080), (1280, 1024)], ids=["1920x180", "1280x1024"])
+def browser_desktop(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
     yield
-
     browser.quit()
 
-@pytest.fixture(scope='function')
-def browser_mobile():
-    browser.config.window_height = 2556
-    browser.config.window_width = 1179
-    browser.config.base_url = 'https://github.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
 
+@pytest.fixture(params=[(375, 665), (430, 932)], ids=["375x665", "430x932"])
+def browser_mobile(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
     yield
-
     browser.quit()
 
 
 def test_github_desktop(browser_desktop):
-    browser.open()
-    browser.element()
+    browser.open("https://github.com/")
+    browser.all('button').element_by(have.text('Sign up')).click()
 
 
 def test_github_mobile(browser_mobile):
-    pass
+    browser.open("https://github.com/")
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-up').click()
